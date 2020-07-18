1d5f56ca13fd8dc78585067448c10485648c14f9
faizkautsar <faiz.kautsar1997@gmail.com>
2020-07-18 19:48:15 +0700

    app_revisi

--- app/Http/Controllers/Karyawan/AuthKaryawanController.php
+++ app/Http/Controllers/Karyawan/AuthKaryawanController.php
@@ -24,7 +24,7 @@ class AuthKaryawanController extends Controller
 
   public function login(Request $request)
   {
-    dd($request->all());
+    // dd($request->all());
       $karyawan = [
         'email' => $request->email,
         'password' => $request->password,
