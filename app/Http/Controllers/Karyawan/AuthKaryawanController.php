34813f2e13fd0eea379f1b37dd81ac701a610c6f
faizkautsar <faiz.kautsar1997@gmail.com>
2020-07-18 19:43:21 +0700

    app_revisi

--- app/Http/Controllers/Karyawan/AuthKaryawanController.php
+++ app/Http/Controllers/Karyawan/AuthKaryawanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Karyawan;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-// use App\Karyawan;
+use App\Karyawan;
 use Auth;
 
 class AuthKaryawanController extends Controller
