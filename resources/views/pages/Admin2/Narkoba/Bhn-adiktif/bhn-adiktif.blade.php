b6c8defb512e370abcd1f2908137c6976406e2d2
faizkautsar <faiz.kautsar1997@gmail.com>
2020-07-27 12:47:27 +0700

    update

--- resources/views/pages/Admin2/Narkoba/Bhn-adiktif/bhn-adiktif.blade.php
+++ resources/views/pages/Admin2/Narkoba/Bhn-adiktif/bhn-adiktif.blade.php
@@ -24,15 +24,15 @@
               <td>{{$bhn_a->nama}}</td>
               <td>{!!str_limit($bhn_a->dampak, 45, '...')!!}</td>
               <td>{!!str_limit($bhn_a->keterangan, 45, '...')!!}</td>
-              <td> <img src="{{asset('uploads/narkoba/BahanAdiktif/'.$bhn_a->gambar)}}" width="100" height="100" alt=""> </td>
+              <td> <img src="{{asset('uploads/narkoba/BahanAdiktif/'.$bhn_a->gambar)}}" width="50" height="50" alt=""> </td>
               <td>
                 <a href="{{route('bhn_adiktif.lihat', $bhn_a->id)}}" class="btn btn-success btn-sm"><i class="fa fa-eye"></i></a>
                 <a href="{{route('bhn_adiktif.ubah', $bhn_a->id)}}" class="btn btn-warning btn-sm"><i class="fa fa-pencil"></i></a>
                 <a href="{{route('bhn_adiktif.hapus', $bhn_a->id)}}" onclick="return confirm('Apakah Anda Yakin Ingin Dihapus?')"
                 class="btn btn-danger btn-sm"><i class="fa fa-remove"></i></a>
               </td>
-            </tr>
             @endforeach
+            </tr>
             </tbody>
           </table>
   </div>
