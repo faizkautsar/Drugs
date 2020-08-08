206eb5e0e899be1cc9b492828205c7fa9e9264f4
faizkautsar <faiz.kautsar1997@gmail.com>
2020-08-08 13:23:43 +0700

    update soft delete

--- app/Laporan.php
+++ app/Laporan.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Laporan extends Model
 {
     protected $guarded = [];
+    use SoftDeletes;
 
     public function user(){
       return $this->belongsTo(User::class, 'id_user','id');
